46c9428066ecb3a34c289fc6b18f376f29b9ffc7
MarLiz <marina.liz.santinicamarena@gmail.com>
2024-08-05 17:33:00 -0600

    added tab bar and options

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() {
   runApp(const MyApp());
 }
 
+List<String> titles = <String>[
+  'Notes',
+  'Lists',
+  'Calendar',
+  'Inventory',
+];
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
@@ -40,32 +47,159 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    //Colors
     var colors = Theme.of(context).extension<AppColors>()!;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: colors.accentuated,
-        title: Text(widget.title),
+    const int tabsCount = 4;
+    return DefaultTabController(
+      initialIndex: 0,
+      length: tabsCount,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: colors.accentuated,
+          title: Text(widget.title),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: const Icon(Icons.library_books_rounded),
+                text: titles[0],
+              ),
+              Tab(
+                icon: const Icon(Icons.list_alt_rounded),
+                text: titles[1],
+              ),
+              Tab(
+                icon: const Icon(Icons.calendar_month_rounded),
+                text: titles[2],
+              ),
+              Tab(
+                icon: const Icon(Icons.inventory_rounded),
+                text: titles[3],
+              )
+            ],
+          ),
+          leading:
+              const Image(image: AssetImage('assets/images/cotidianis.png')),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return const ListTile(
+                  title: Text('MyLists'),
+                );
+              },
+            ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return const ListTile(
+                  title: Text('Calendar'),
+                );
+              },
+            ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return const ListTile(
+                  title: Text('Notes'),
+                );
+              },
+            ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return const ListTile(
+                  title: Text('Notes'),
+                );
+              },
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Column(
+    );
+    /*
+      appBar: AppBar(
+          backgroundColor: colors.accentuated,
+          title: Text(widget.title),
+          scrolledUnderElevation: 4.0,
+          shadowColor: Theme.of(context).shadowColor,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: const Icon(Icons.cloud_outlined),
+                text: 'Notes',
+              ),
+              Tab(
+                icon: const Icon(Icons.beach_access_sharp),
+                text: 'My lists',
+              ),
+              Tab(
+                icon: const Icon(Icons.brightness_5_sharp),
+                text: 'Calendar',
+              ),
+            ],
+          ),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          )),
+      body: TabBarView(
+        children: <Widget>[
+          ListView.builder(
+            itemCount: 25,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                tileColor: colors.accentuated,
+                title: Text('MyLists'),
+              );
+            },
+          ),
+          ListView.builder(
+            itemCount: 25,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                tileColor: colors.accentuated,
+                title: Text('Calendar'),
+              );
+            },
+          ),
+          ListView.builder(
+            itemCount: 25,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                tileColor: colors.accentuated,
+                title: Text('Notes'),
+              );
+            },
+          ),
+        ],
+      ),
+      /*child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'TODO: Buttons goes here',
-              //TODO: put the buttons in here
+              //T: put the buttons in here
             ),
             Text(
               ':)',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
-        ),
-      ),
-      /*floatingActionButton: FloatingActionButton(
+        ),*/
+    );
+    floatingActionButton: FloatingActionButton(
         //onPressed:
         tooltip: 'Create',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.*/
-    );
   }
 }
